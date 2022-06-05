a78df0722b8a69d3ba664895e9dda62e178b4738
Wanderley <100Palavr@gmail.com>
2022-06-05 16:52:13 -0300

    Update and rename compress.cpp to compress.c

--- compress.c
+++ compress.c
@@ -5,13 +5,15 @@
 
 #define min(a, b) (a < b ? a : b)
 
+typedef unsigned char byte;
+
 const int search_buf    = 1024;
 const int lookahead_buf = 100;
 
 typedef struct tuple {
     unsigned short backward;
-    unsigned char  len;
-    unsigned char  symbol;
+    byte len;
+    byte symbol;
 } tuple;
 
 typedef struct dynamic_list {
@@ -19,6 +21,10 @@ typedef struct dynamic_list {
     int    capacity;
     int    len;
 } dynamic_list;
+
+void free_list(dynamic_list list) {
+	free(list.buf);
+}
 
 dynamic_list inicialize(int capacity) {
     dynamic_list aux;
@@ -35,11 +41,11 @@ dynamic_list inicialize(int capacity) {
     return aux;
 }
 
-tuple make_tuple(int arg1, int arg2, unsigned char arg3) {
+tuple make_tuple(unsigned short arg1, byte arg2, byte arg3) {
     tuple ret;
     ret.backward = arg1;
     ret.len      = arg2;
-    ret.symbol        = arg3;
+    ret.symbol   = arg3;
     return ret;
 }
 
@@ -59,71 +65,19 @@ void insert(dynamic_list* list, tuple arg) {
         free(list->buf);
 
         // updating new capacity
-        list->capacity = list->capacity * 2;
-        list->buf      = resized_buf;
+        list->capacity       = list->capacity * 2;
+        list->buf            = resized_buf;
+		list->buf[list->len] = arg;
     }
 
     list->len++;
 }
 
-int traverse_list(dynamic_list list) {
-    int cnt_bytes = 0;
+void list_tuples(dynamic_list list) {
     for(int i = 0; i < list.len; i++) {
-        printf("(%d, %d, %c) ", list.buf[i].backward, list.buf[i].len, list.buf[i].symbol);
-        cnt_bytes += 4;
+        printf("(%d, %c, %c) ", list.buf[i].backward, list.buf[i].len, list.buf[i].symbol);
     }
     printf("\n");
-
-    return cnt_bytes;
-}
-
-dynamic_list compression(unsigned char buf[], int len) {
-    dynamic_list list;
-
-    list = inicialize(1024);
-
-    insert(&list, make_tuple(0, 0, buf[0]));
-
-    for(int i = 1; i < len; i++) {
-        tuple temp = make_tuple(0, 0, buf[i]);
-        for(int j = i-1; j >= 0 && j >= i-search_buf; j--) {
-            int cx = j;
-            for(int p = i; p < min(i+lookahead_buf, len) && cx < i; p++) {
-                if(buf[cx] == buf[p]) {
-                    if(temp.len < p-i+1) {
-                        temp = make_tuple(i-j, p-i+1, buf[p+1 == len ? p : p+1]);
-                    }
-                    cx++;
-                } else {
-                    break;
-                }
-            }
-        }
-
-        insert(&list, temp);
-        i += temp.len ? temp.len : 0;
-    }
-
-    return list;
-}
-
-int read_binary(unsigned char** buf, const char* in) {
-    FILE* fp = fopen(in, "rb");
-
-    if(fp == NULL) {
-        printf("Erro na alocacao de memoria\n");
-        return -1;
-    }
-
-    fseek(fp, 0L, SEEK_END);
-    int len = ftell(fp);
-    rewind(fp);
-
-    *buf = (unsigned char*)malloc(sizeof(unsigned char) * len);
-
-    fread(*buf, len, 1, fp);
-    fclose(fp);
-    return len;
 }
 
 void write_binary(dynamic_list list, const char* out) {
@@ -138,23 +92,64 @@ void write_binary(dynamic_list list, const char* out) {
     fclose(fp);
 }
 
-int main() {
-    unsigned char* buf;
+int read_binary(byte** buf, const char* in) {
+    FILE* fp = fopen(in, "rb");
+
+    if(fp == NULL) {
+        printf("Erro na alocacao de memoria\n");
+        return -1;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    int len = ftell(fp);
+    rewind(fp);
+
+    *buf = (byte*)malloc(sizeof(byte) * len);
+
+    fread(*buf, len, 1, fp);
+    fclose(fp);
+    return len;
+}
+
+void compression(byte buf[], int len, const char* out) {
+    dynamic_list list;
+
+    list = inicialize(1024);
+
+    insert(&list, make_tuple(0, 0, buf[0]));
+
+    for(int i = 1; i < len; i++) {
+        tuple temp = make_tuple(0, 0, buf[i]);
+        for(int j = i-1; j >= 0 && j >= i-search_buf; j--) {
+            int cx = j;
+            for(int p = i; p < min(i+lookahead_buf, len) && cx < i; p++) {
+                if(buf[cx] == buf[p]) {
+                    if(temp.len < p-i+1) {
+                        temp = make_tuple(i-j, p-i+1, buf[p+1 >= len ? p : p+1]);
+                    }
+                    cx++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        insert(&list, temp);
+        i += temp.len ? temp.len : 0;
+    }
+
+    write_binary(list, out);
+	printf("%ld compressed bytes\n", sizeof(tuple) * list.len);
+	free_list(list);
+}
+
+int main() {	
+    byte* buf;
 
     int n = read_binary(&buf, "vinho.csv");
 
-    printf("%d\n", n);
+	printf("%d uncompressed bytes\n", n);
 
-    dynamic_list list = compression(buf, n);
-    int bt = traverse_list(list);
-
-    write_binary(list, "saida.out");
-
-    printf("%d compressed bytes vs %d uncompressed bytes\n", bt, n);
+    compression(buf, n, "saida.out");
     return 0;
 }
-
-// a b a b c b a b a b a a b
-
-// (0, 0, 'a'), (0, 0, 'b'), (2, 2, '*'), (0, 0, 'c'), (4, 3, '*'), (8, 3, '*'), (1, 1, '*')
-// (0, 0, 'a'), (0, 0, 'b'), (2, 2, 'c'), (4, 3, 'a'), (2, 2, 'a')
